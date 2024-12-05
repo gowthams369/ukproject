e05715f887d7ae3ec96800c152517ab674c5aca0
Gowtham <gowthamsasikumar369@gmail.com>
2024-12-05 12:34:12 +0530

    third commit

--- controller/userController.js
+++ controller/userController.js
@@ -1,4 +1,4 @@
-import bcrypt from "bcrypt.js";
+import bcrypt from "bcryptjs";
 import asyncHandler from "../middlewares/asyncHandler.middleware.js";
 import { User } from "../model/userModel.js";
 import AppError from "../utils/error.util.js";
--- model/userModel.js
+++ model/userModel.js
@@ -1,6 +1,5 @@
 import mongoose from "mongoose";
 import jwt from "jsonwebtoken";
-import bcrypt from "bcrypt";
 
 // Define the user schema
 const userSchema = new mongoose.Schema(
